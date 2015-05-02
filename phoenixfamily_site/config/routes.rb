296adf515f13cecd20f5f4ba72c41ab5b119ae7c
josiah14 <praenato14@gmail.com>
2015-05-02 17:43:51 -0500

    add stories resource, route, and index page

--- phoenixfamily_site/config/routes.rb
+++ phoenixfamily_site/config/routes.rb
@@ -1,4 +1,5 @@
 PhoenixfamilySite::Application.routes.draw do
+  resources :stories
   root "pages#home"
   get "home", to: "pages#home", as: "home"
   get "about", to: "about_pages#about", as: "about"
@@ -6,8 +7,8 @@ PhoenixfamilySite::Application.routes.draw do
   get "/contact", to: "pages#contact", as: "contact"
   post "/emailconfirmation", to: "pages#email", as: "email_confirmation"
 
-  get "posts", to: "pages#posts", as: "posts"
-  get "posts/:id", to: "pages#show_post", as: "post"
+  # get "stories", to: "pages#posts", as: "posts"
+  # get "posts/:id", to: "pages#show_post", as: "post"
   devise_for :users
 
   namespace :admin do
