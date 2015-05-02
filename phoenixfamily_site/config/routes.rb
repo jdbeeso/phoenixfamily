e228993e9098b68d260a8f11ae60232e5c662fcf
josiah14 <praenato14@gmail.com>
2015-05-02 17:02:14 -0500

    create about pages route

--- phoenixfamily_site/config/routes.rb
+++ phoenixfamily_site/config/routes.rb
@@ -1,11 +1,11 @@
 PhoenixfamilySite::Application.routes.draw do
   root "pages#home"
   get "home", to: "pages#home", as: "home"
-  get "about", to: "about_pages#about"
+  get "about", to: "about_pages#about", as: "about"
   get "inside", to: "pages#inside", as: "inside"
   get "/contact", to: "pages#contact", as: "contact"
   post "/emailconfirmation", to: "pages#email", as: "email_confirmation"
-  
+
   get "posts", to: "pages#posts", as: "posts"
   get "posts/:id", to: "pages#show_post", as: "post"
   devise_for :users
