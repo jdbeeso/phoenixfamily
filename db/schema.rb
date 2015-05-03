54c5fee9a40e5933f6dc2187dfa450f72e462272
josiah14 <praenato14@gmail.com>
2015-05-02 21:57:39 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502222708) do
+ActiveRecord::Schema.define(version: 20150502223430) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150502222708) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "stories", force: :cascade do |t|
+    t.text     "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "successes", force: :cascade do |t|
     t.text     "content"
